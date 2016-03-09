cdd4a07137f5b9be363c415eddf07dafd6e5f646
Michael Teagle <mkteagle@gmail.com>
2016-03-09 16:36:44 -0700

    made a  ton of changes

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
-
+angular.module('starter', ['ionic', 'starter.controllers', 'app.ctrl', 'ngToast', 'app.login', 'firebase', 'gameService', 'gameController', 'upgradeDirective', 'nameFilters', 'angular-toArrayFilter'])
+  .constant('firebaseUrl', "https://donutclick2.firebaseio.com/")
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -29,14 +29,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    controller: 'AppCtrl as ac'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.leaderboard', {
+    url: '/leaderboard',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/leaderboard.html'
       }
     }
   })
@@ -44,7 +44,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       url: '/login',
       views: {
         'menuContent': {
-          templateUrl: 'templates/login.html'
+          templateUrl: 'templates/login.html',
+          controller: 'loginController as lc'
+        }
+      }
+    })
+
+    .state('app.game', {
+      url: '/game',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/game.html',
+          controller: 'gameController as gc'
         }
       }
     })
@@ -77,5 +88,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/login');
 });
